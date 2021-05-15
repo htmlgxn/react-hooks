acf4f947237f3b0263c54f110de6ae6ec5db2773
Ben Chitty <htmlgxn@protonmail.com>
2021-05-15 16:49:31 -0300

    Imported useState hook

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+	useState();
+
+	return (
+		<div>
+			<ul>
+				<li>Pay bills</li>
+				<li>Do your homework</li>
+				<li>Feed the dog</li>
+			</ul>
+		</div>
+	)
+};
+
+export default TodoList;
